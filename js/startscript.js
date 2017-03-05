fc5c46f107089d7ce0f24a4b871a92a84b9537c9
MattOxberry <tuc19266@temple.edu>
2017-03-05 10:14:40 -0500

    js function

--- js/startscript.js
+++ js/startscript.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-   
+   $(".dateFormat").click(function() {
+        var date = new Date();
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var ampm = date.getHours() < 12 ? "am" : "pm";
+        var t = date.getHours() + ":" + date.getMinutes() + ampm;
+        
+        return 
+   })
 });
