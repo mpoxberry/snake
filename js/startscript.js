7be2b10d6659ce9506ba379062f8d319e10673ac
MattOxberry <tuc19266@temple.edu>
2017-03-05 12:32:47 -0500

    added some basic js functions

--- js/startscript.js
+++ js/startscript.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
-   $(".dateFormat").click(function() {
-        var date = new Date();
-        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var ampm = date.getHours() < 12 ? "am" : "pm";
-        var t = date.getHours() + ":" + date.getMinutes() + ampm;
-        
-        return 
-   })
-});
+//var date = new Date();
+var msg;
+
+//Date Format
+function dateFormat(date) {
+    var date = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var today = days[date.getDay()];
+    var thisDate = date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear();
+    var ampm = date.getHours() < 12 ? "am" : "pm";
+    var t = date.getHours() + ":" + date.getMinutes() + ampm;
+
+    var o = t + " " + today + " " + thisDate;
+    document.getElementById("clock").textContent = o;
+    //greeting(date);
+};
+
+function greeting() {
+    var date = new Date();
+    var ampm = date.getHours() < 12 ? "am" : "pm";
+    if (ampm === "am") {
+        msg = "Top of the moring!";
+    } else {msg = "Good night."}
+
+    var s = msg;
+    document.getElementById("greeting").textContent = s;
+};
